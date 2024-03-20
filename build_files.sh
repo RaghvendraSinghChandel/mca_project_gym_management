0bc86fa0fa61d839e4d4219d54794aeda869a3b7
Raghvendra Singh <sundaramsingh8924@gmail.com>
2024-03-20 11:48:51 +0530

    change sh file

--- build_files.sh
+++ build_files.sh
@@ -9,11 +9,21 @@ handle_error() {
 # Activate the virtual environment
 source /Users/clanap.technologies/Downloads/gymTest/myenv/bin/activate
 
+# Check if dotenv is installed
+if ! python -c "import dotenv" &> /dev/null; then
+    echo "dotenv is not installed. Installing dotenv..."
+    pip install python-dotenv || handle_error "Failed to install dotenv"
+fi
+
 # Check if Django is installed
 if ! python -c "import django" &> /dev/null; then
     echo "Django is not installed. Installing Django..."
     pip install django || handle_error "Failed to install Django"
 fi
+
+# Install Python dependencies
+echo "Installing Python dependencies from requirements.txt..."
+pip install -r requirements.txt || handle_error "Failed to install Python dependencies"
 
 # Run database migration script
 echo "Applying database migrations..."
@@ -22,10 +32,6 @@ python3.9 manage.py migrate || handle_error "Database migration failed"
 # Run fitness.sql to create required tables
 echo "Creating tables from fitness.sql..."
 mysql -h "localhost" -u "root" -p"raghvendra" "gymdb" < fitness.sql || handle_error "Failed to execute fitness.sql"
-
-# Install Python dependencies
-echo "Installing Python dependencies..."
-pip install -r requirements.txt || handle_error "Failed to install Python dependencies"
 
 # Collect static files
 echo "Collecting static files..."
